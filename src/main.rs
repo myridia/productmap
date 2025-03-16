d9e5aaf0eecca473c11f2488181b0601729cfd30
veto <veto@lab.local>
2025-03-16 15:02:07 +0700

    works_with_categories_and_products

--- src/main.rs
+++ src/main.rs
@@ -20,49 +20,55 @@ async fn main() -> Result<(), Box<dyn Error>> {
         }
     };
 
-    let _path_index = &format!("website/public/index.html");
-    let path_index = Path::new(&_path_index);
+    let _path_home = &format!("website/public/home.html");
+    let path_home = Path::new(&_path_home);
 
-    let _path_sku = &format!("website/public/sku");
+    let _path_product = &format!("website/public/product");
 
-    let path_sku = Path::new(&_path_sku);
-    if !path_sku.exists() {
-        fs::remove_dir_all(&_path_sku)?;
-        fs::create_dir(_path_sku)?;
+    let path_product = Path::new(&_path_product);
+    if path_product.exists() {
+        fs::remove_dir_all(&_path_product)?;
+    }
+    if !path_product.exists() {
+        fs::create_dir(_path_product)?;
     }
     let _path_category = &format!("website/public/category");
     let path_category = Path::new(&_path_category);
-    if !path_category.exists() {
+    if path_category.exists() {
         fs::remove_dir_all(&_path_category)?;
+    }
+    if !path_category.exists() {
         fs::create_dir(_path_category)?;
     }
 
-    let mut v = vec!["".to_string()];
+    let mut categories = vec![];
 
-    let mut context = Context::new();
-    for i in 1..120 {
-        let mut context = Context::new();
-        v.push(i.to_string());
-        context.insert("sku", &i.to_string());
-        let r = tera.render("sku.html", &context)?;
-        let path = &format!("website/public/sku/{0}.html", i);
+    for i in 1..8 {
+        let mut products = vec![];
+        categories.push(format!("/category/category_{i}.html"));
+        for ii in 1..11 {
+            let mut context_product = Context::new();
+            products.push(format!("/product/product_{i}_{ii}.html"));
+            context_product.insert("product", &format!("product_{i}_{ii}.html"));
+            let r = tera.render("product.html", &context_product)?;
+            let path = &format!("website/public/product/product_{0}_{1}.html", i, ii);
+            let mut output = File::create(path)?;
+            let _w = write!(output, "{}", r);
+        }
+
+        let mut context_category = Context::new();
+        context_category.insert("products", &products);
+        let r = tera.render("category.html", &context_category)?;
+        let path = &format!("website/public/category/category_{0}.html", i);
         let mut output = File::create(path)?;
         let _w = write!(output, "{}", r);
     }
 
-    let mut context1 = Context::new();
-    for i in 1..12 {
-        let mut context1 = Context::new();
-        let r = tera.render("category.html", &context)?;
-        let path = &format!("website/public/category/{0}.html", i);
-        let mut output = File::create(path)?;
-        let _w = write!(output, "{}", r);
-    }
-
-    let mut context2 = Context::new();
-    context2.insert("v", &v);
-    let r = tera.render("index.html", &context2)?;
-    let mut output = File::create(path_index)?;
+    let mut context_home = Context::new();
+    context_home.insert("categories", &categories);
+    let r = tera.render("home.html", &context_home)?;
+    let mut output = File::create(path_home)?;
     let _w = write!(output, "{}", r);
+
     Ok(())
 }
